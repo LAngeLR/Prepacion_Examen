c4d32d7135f0b3d7d360d419efebf1b3db3fe193
LuisAngel <a20192832@pucp.edu.pe>
2024-07-09 22:06:47 -0500

    BackupClases

--- Eureka_Client/ClientEurekaCalculator/src/main/java/com/example/clienteurekacalculator/CalculatorController.java
+++ Eureka_Client/ClientEurekaCalculator/src/main/java/com/example/clienteurekacalculator/CalculatorController.java
@@ -1,6 +1,8 @@
 package com.example.clienteurekacalculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,22 @@ public class CalculatorController {
     @Autowired
     private ServiceAdditionClient serviceAdditionClient;
 
+    final CircuitBreakerFactory circuitBreakerFactory;
+
+    public  CalculatorController(CircuitBreakerFactory circuitBreakerFactory) {
+        this.circuitBreakerFactory = circuitBreakerFactory;
+    }
+
     @GetMapping("/addcalculator/{item_a}/{item_b}")
     public String addCalculator(@PathVariable("item_a") int item_a,
                                 @PathVariable("item_b") int item_b)
     {
-        return "Calculator : " + serviceAdditionClient.addition(item_a,item_b);
+        CircuitBreaker breaker = circuitBreakerFactory.create("addcalculator");
+
+        return breaker.run(() -> {
+            return "Calculator : " + serviceAdditionClient.addition(item_a,item_b);
+        }, throwable -> {
+            return "Calculator : Wait";
+        });
     }
 }
